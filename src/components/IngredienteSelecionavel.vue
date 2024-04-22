49b442404203424ac28031740263124e81dec4ce
Emanuel Lima <limaemanuel773@gmail.com>
2024-04-22 12:19:50 -0300

    Initial commit

---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import Tag from './Tag.vue';
+export default {
+    components: { Tag },
+    props: {
+        ingrediente: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return{
+            selecionado: false
+        }
+    },
+    methods: {
+        aoClicar() {
+           this.selecionado = !this.selecionado
+
+           if (this.selecionado) {
+            this.$emit('AdicionarIngrediente', this.ingrediente)
+           } else {
+            this.$emit('RemoverIngrediente', this.ingrediente);
+           }
+        }
+    },
+    emits: ['AdicionarIngrediente', 'RemoverIngrediente']
+}
+</script>
+
+
+<template>
+    <button
+    class="ingrediente"
+    @click="aoClicar"
+    :aria-pressed="selecionado"
+    >
+        <Tag :texto="ingrediente" :ativa="selecionado"/>
+    </button>
+</template>
+
+<style scoped>
+.ingrediente{
+    cursor: pointer;
+}
+</style>
